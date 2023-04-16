f3c3cfca7be78449910db0713583f60f9e94ed0b
Vivek_Kumar_Patel <vivekkrpatel2442@gmail.com>
2023-04-16 22:37:17 +0530

    call method to send email

---
+++ Main.java
@@ -0,0 +1,9 @@
+package org.geekster;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HandleMail mailer = new HandleMail();
+        mailer.sendAttachment();
+    }
+}
