04fc0226a348859caf7cc4bc4e4f3ae0aa7befe5
Vivek_Kumar_Patel <vivekkrpatel2442@gmail.com>
2023-04-16 22:34:05 +0530

    Created constants final such as message,subject,emailIds

---
+++ MailConstants.java
@@ -0,0 +1,13 @@
+package org.geekster;
+
+public class MailConstants {
+
+    public static final String SENDER = "vivekkrpatel2442@gmail.com";
+
+    public static final String RECEIVERMAILADDRESS = "mainakgh1@gmail.com";
+
+    public static final String SUBJECT = "Testing java Mail : 16 April 2023";
+
+    public static final String MESSAGE = "Sir ,I like the way you teach.Thank you so much sir";
+
+}
